3811725e06f390d9e2e5b3e234435cd6607eec28
VPavliashvili <v_pavliashvili@yahoo.com>
2023-05-27 19:10:39 +0400

    sorting test refactored

--- sorts/bubblesort.go
+++ sorts/bubblesort.go
@@ -4,7 +4,7 @@ package sorts
 // starts with first and comparing it to the next
 // if current value is greater then next
 // both switches their indexes in the array
-func bubbleSort(list []int) []int {
+func BubbleSort(list []int) []int {
 	for i, k := 0, len(list)-1; i < len(list)-1; i++ {
 		for j := 0; j < k; j++ {
 			cur := list[j]
--- sorts/sorts_test.go
+++ sorts/sorts_test.go
@@ -1,39 +1,44 @@
-package sorts
+package sorts_test
 
 import (
-	"reflect"
+	"algorithmsAndDataStructures/sorts"
 	"testing"
+
+	"github.com/gookit/goutil/testutil/assert"
 )
 
-func TestBubble(t *testing.T) {
+func Test(t *testing.T) {
 	cases := []struct {
 		list []int
 		want []int
+        function func([]int) []int
 	}{
 		{
 			list: []int{5, 2, 10, 7, 220, 22, 1},
 			want: []int{1, 2, 5, 7, 10, 22, 220},
+            function: sorts.BubbleSort,
 		},
 		{
 			list: []int{5, 2, 10, 7, 220, 22, 1, 150},
 			want: []int{1, 2, 5, 7, 10, 22, 150, 220},
+            function: sorts.BubbleSort,
 		},
 		{
 			list: []int{1},
 			want: []int{1},
+            function: sorts.BubbleSort,
 		},
 		{
 			list: []int{},
 			want: []int{},
+            function: sorts.BubbleSort,
 		},
 	}
 
 	for _, item := range cases {
-		got := bubbleSort(item.list)
+		got := item.function(item.list)
 		want := item.want
 
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("error in bubbleSort\ngot\n%v\nwant\n%v", got, want)
-		}
+        assert.Equal(t, want, got)
 	}
 }
